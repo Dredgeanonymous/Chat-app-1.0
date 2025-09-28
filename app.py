d261ce7c793cec6018f6315178f5736c073279af
Dredgeanonymous <anthonydelvin@gmail.com>
2025-09-28 12:15:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,15 @@ import os
 from datetime import datetime, timezone
 from flask import Flask, render_template, request, redirect, url_for, session, send_from_directory, jsonify
 from flask_socketio import SocketIO, emit, disconnect
-app = Flask(__name__, static_folder="static", template_folder="templates")
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "change-me")
+from flask_socketio import SocketIO, emit, disconnect
 
 socketio = SocketIO(
     app,
     cors_allowed_origins="*",
-    async_mode="gevent",      # using eventlet (matches your Procfile)
+    async_mode="gevent",      # <-- switch from "eventlet" to "gevent"
     logger=True,
     engineio_logger=True,
-    message_queue=os.environ.get("REDIS_URL")  # OK if None
+    message_queue=os.environ.get("REDIS_URL")  # ok if None
 )
 
 MOD_CODE = os.environ.get("MOD_CODE", "12345")
