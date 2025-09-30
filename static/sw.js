22f22f4d18580522816ae2175b888acedd58b019
Dredgeanonymous <anthonydelvin@gmail.com>
2025-09-30 07:15:25 -0400

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -6,4 +6,7 @@ self.addEventListener('activate', (e) => {
   // Claim clients so the SW is active immediately.
   e.waitUntil(self.clients.claim());
 });
+
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
 // (Optional) add fetch handler if you want offline caching later.
